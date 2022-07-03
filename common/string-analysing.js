b9bc9f6735971ff7dbad97bd3709e39164a11ebd
Виталий <71096582+ptsmor@users.noreply.github.com>
2022-07-03 17:42:11 +0300

    Update string-analysing.js

--- common/string-analysing.js
+++ common/string-analysing.js
@@ -1,13 +1,13 @@
 const path = "/users/download/index.html"
 
 const isHtml = path => {
-// передаем строку эмулирующую путь до файла
+
 const requiredExt = ".html";
-// создали переменную с которой будем сравнивать строку
+
 const pathExt = path.slice(-5);
-// создадим переменную в которую запишем вырезаную строку (-5 это 5 с конца)
+
 return requiredExt == pathExt
-// == всегда возвращает true или false
+
 }
-// если равны то true иначе false
+
 console.log(isHtml(path));
